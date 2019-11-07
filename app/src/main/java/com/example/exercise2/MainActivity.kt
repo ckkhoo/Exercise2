0a32e0ee6e7e72b14c4839ceab464bd29e088ad5
ckkhoo <khoock-wm17@student.tarc.edu.my>
2019-11-07 10:00:10 +0800

    Initial

---
+++ app/src/main/java/com/example/exercise2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.exercise2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(){
+            calculateBMI();
+        }
+    }
+
+    private fun calculateBMI(){
+        val weight = editTextWeight.text.toString().toInt();
+        val height = editTextHeight.text.toString().toFloat();
+
+        val bmi = weight / height.pow(2);
+
+        textViewBMI.text = getString(R.string.bmi) + ": " + String.format("%.2f", bmi);
+        if (bmi < 18.5) {
+            imageViewBMI.setImageResource(R.drawable.under);
+            textViewStatus.text = getString(R.string.status) + ": Underweight"
+        } else if (bmi < 25) {
+            imageViewBMI.setImageResource(R.drawable.normal);
+            textViewStatus.text = getString(R.string.status) + ": Normal";
+        } else {
+            imageViewBMI.setImageResource(R.drawable.over);
+            textViewStatus.text = getString(R.string.status) + ": Overweight";
+        }
+    }
+}
